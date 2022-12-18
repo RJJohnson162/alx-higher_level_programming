3365ed9b8f60ad527100a5fe9c6d1a11b439cc6f
Marubi N Richard <91172411+RJJohnson162@users.noreply.github.com>
2022-12-19 00:57:54 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
-        
+if __name__ == '__main__':
+  import sys
+  argv = sys.argv[1:] # remove the script name from the list of arguments
+  num_args = len(argv)
+  if num_args == 0:
+    print("No argument.")
+  elif num_args == 1:
+    print("1 argument:")
+  else:
+    print(f"{num_args} arguments:")
+  for i, arg in enumerate(argv):
+    print(f"{i+1}: {arg}")
